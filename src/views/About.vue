e1f0081c4622ee233a917a1f817e82b720380666
Andre Dargains <andre.dargains@gmail.com>
2018-11-27 17:59:50 +0000

    ADD: vue-router;

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <section>
+    <h1>About</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <section>
+    <h1>Home</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style scoped >
+  h1 {
+    font-size: 40px;
+  }
+</style>
